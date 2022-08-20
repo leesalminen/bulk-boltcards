d976f89df81d391fbdd03efaf0a05052bc0d4e73
Lee Salminen <leesalminen@gmail.com>
2022-08-20 17:44:54 -0600

    open in incognito window

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # php -f create.php $1 | base64 | (read data && sed -e "s/var data_string = .*/var data_string = \"$data\"/" ./template.html > ./temp.html) && open -a "Google Chrome.app" temp.html && sleep 2 && rm temp.html
 
-php -f create.php $1 | base64 | (read data && sed -e "s/var data_string = .*/var data_string = \"$data\"/" ./template.html) | base64 | (read data && open -a "Google Chrome.app" "data:text/html;base64,$data")
+php -f create.php $1 | base64 | (read data && sed -e "s/var data_string = .*/var data_string = \"$data\"/" ./template.html) | base64 | (read data && open -a "Google Chrome.app" "data:text/html;base64,$data" --args --incognito)
