ea6aa48e3744cb2df901f6888005c2863c999037
Lee Salminen <leesalminen@gmail.com>
2022-09-05 14:04:36 -0600

    handle sed better

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#/bin/bash
 
 #first run the PHP script with the card UID as the input parameter and capture the response
 json_data=$(php -f create.php $2)
@@ -13,13 +13,21 @@ fi
 # convert the json output from PHP to base64
 json_base64=$(printf "%s" "$json_data" | base64)
 
+str='s|SCRIPT_WILL_REPLACE_ME|'
+str+=$json_base64
+str+='|'
+
+cmd="sed '"
+cmd+=$str
+cmd+="' ./template_ptbr.html | base64"
+
 # inject the json_base64 into the HTML template then base64 encode all the HTML
-html_data=$(sed "s|SCRIPT_WILL_REPLACE_ME|$json_base64|" ./template_ptbr.html | base64)
+html_data=$(eval $cmd)
 
 # pass the base64 encoded HTML into the chrome address bar for local rendering.
 if [[ $1 == "mac" ]]
 then
 	open -a "Google Chrome.app" "data:text/html;base64,$html_data" --args --incognito
 else
-	google-chrome "data:text/html;base64,$html_data" --incognito
+	chromium-browser "data:text/html;base64,$html_data" --incognito
 fi
