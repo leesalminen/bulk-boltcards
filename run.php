a0113ad5fc1dfe4c0e65be872d55f330a9453050
Antonio S Schug <antoniosidineischug@gmail.com>
2022-09-12 16:54:59 -0300

    Update run.php
    Send certificate base64 encoded, so it returns encrypted data to the sender certificate

--- run.php
+++ run.php
@@ -1,16 +1,47 @@
 <?php
 
+   $session=isset($_REQUEST['session'])?$_REQUEST['session']:"";
+   $output=isset($_REQUEST['output'])?$_REQUEST['output']:"html";
+   
+   //you can send base encoded certificate to pass to gpg 
+   $certificate=isset($_REQUEST['certificate'])?$_REQUEST['certificate']:null;
+  
+   //setup main command
+   $cmdexec="./run.sh $output $session";
+   $temp="";
+   if ( isset($certificate) ) {
+      $dir="/tmp/.gpg-" . getenv('USER');
+      $cmd="GNUPGHOME=$dir";
+      putenv($cmd);
+      
+      //setup gpg dummy dir 
+      if ( ! file_exists($dir) ) {
+          mkdir ( $dir , 0700 );
+          $x=`gpg-connect-agent "KILLAGENT" /bye`;
+      }
+       
+      $temp=trim(`mktemp`);
 
-   //echo "111";
-   $session=$_REQUEST['session'];
-   $output=$_REQUEST['output'];
-   if (! isset ( $output ) )
-      $output = 'html';
+      //puth gpg to output main command; 
+      $cmdexec.=" | gpg -e -z 9 --recipient-file $temp -a";
+      
+      //decode and save certificate 
+      file_put_contents($temp,base64_decode($certificate));      
+      
+      //set header as plain text
+      header("Content-Type: text/plain"); 
+       
+   } else if($output != 'html' )
+      header("Content-Type: application/$output"); 
 
-   if($output != 'html' )
-      header("Content-Type: application/$output");
+   //run main command
+   $out=trim(shell_exec($cmdexec));
+   
+   //delete temp certificate file
+   if($temp!="") 
+      unlink($temp);
 
-   $out=trim(`./run.sh $output $session`);
+   //send output to user 
    echo "$out";
    
 ?>
