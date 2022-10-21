4a90f54026162303ddf69f8b44fe8f6a1cbfcf39
Lee Salminen <leesalminen@gmail.com>
2022-10-21 15:00:17 -0600

    dont commit

--- constants.php
+++ constants.php
@@ -47,9 +47,9 @@ define("BOLT_GENERATOR_CODE", "https://github.com/praiabitcoin/bulkcards");
 define("IMPLEMENTATION_GUIDE", "https://github.com/praiabitcoin/naobanco");
 
 // to use Pipefy integration to store details about generated cards, generate an access token
-define("PIPEFY_TOKEN", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDIxODY0MzksImVtYWlsIjoibGVlc2FsbWluZW5AZ21haWwuY29tIiwiYXBwbGljYXRpb24iOjMwMDIwNzcyM319.JFFbYYYTGfPCQzylCZBWxq8UDJyvehIacyOohKg1prLtRo8y_NssZzZeCkPgV2jqAXAab1cO9TTsxFWIAHZl8A");
-define("PIPEFY_ID", "302766835");
-define("PIPEFY_CATEGORY", "307790751");
+define("PIPEFY_TOKEN", "");
+define("PIPEFY_ID", "");
+define("PIPEFY_CATEGORY", "");
 
 //Define a SOCKS5 proxy, if you desire it 
 //define("SOCKS5", "192.168.0.201:9050");
